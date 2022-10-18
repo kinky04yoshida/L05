d24f5bd261e1457cc4fdb706202a7b3781387922
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-18 10:46:22 +0000

    SyntaxError (/home/ubuntu/environment/L05/twitter/app/controllers/users_controller.rb:8: syntax error, unexpected end-of-input):

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'top/main'
   resources :users
   root 'users#index'
-  get 'users/new_user', to: 'users#new_user'
+  get 'users/new_user', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
