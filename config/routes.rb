29ec58930eeba5b8e7f6b83ee0f05a2e42511753
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-18 13:35:46 +0000

    Completed 200 OK in 59ms (Views: 53.8ms | ActiveRecord: 0.7ms)

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   post 'tweets', to: 'tweets#create'
   delete 'tweets/:id', to: 'tweets#destroy'
   get 'tweets/:id', to: 'tweets#show'
+  get 'users/list', to: 'users#list'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
