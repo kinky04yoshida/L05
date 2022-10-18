0d77ef4ae695851647e78dd84089aca8e77bbabc
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-18 17:55:22 +0000

    Completed 200 OK in 370ms (Views: 357.2ms | ActiveRecord: 1.7ms)

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,35 @@
 class TopController < ApplicationController
+
   def main
+    if session[:login_uid]
+      render "main"
+    else
+      render "login"
+    end
+  end
+    
+  def top
+    @users = User.all
+  end
+
+  def login
+    input_pass = params[:pass]
+    if User.find_by(uid: params[:uid])
+      user = User.find_by(uid: params[:uid])
+      db_pass = user.pass
+      if BCrypt::Password.new(db_pass) == input_pass
+        session[:login_uid] = params[:uid]
+        redirect_to root_path
+      else
+        render 'error'
+      end
+    else
+      render 'error'
+    end
+  end
+    
+  def logout
+    session.delete(:login_uid)
+    redirect_to root_path
   end
 end
