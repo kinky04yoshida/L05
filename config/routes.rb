06d1a41574ce177219f5b837a91cc3dc9d9358fe
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-18 13:39:02 +0000

    Completed 200 OK in 94ms (Views: 90.5ms | ActiveRecord: 0.8ms)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   get 'top/main'
-
+  resources :users
   resources :tweets
   root 'users#index'
   get 'tweets/new', to: 'tweets#new'
   post 'tweets', to: 'tweets#create'
   delete 'tweets/:id', to: 'tweets#destroy'
-  get 'tweets/:id', to: 'tweets#show'
   get 'users/list', to: 'users#list'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
