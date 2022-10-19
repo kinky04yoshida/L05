f57a046a2ff46040758f7cf19f56ba3bd37eb545
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-19 07:42:40 +0000

    Completed 200 OK in 135ms (Views: 122.8ms | ActiveRecord: 0.0ms)

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -19,7 +19,7 @@ class TopController < ApplicationController
       db_pass = user.pass
       if BCrypt::Password.new(db_pass) == input_pass
         session[:login_uid] = params[:uid]
-        @login_user = user.pass
+        @login_user = input_pass
         redirect_to root_path
       else
         render 'error'
