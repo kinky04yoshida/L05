56d76f49e8e1c004b2b4fa78b32c8513cb5d85f8
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-19 07:08:43 +0000

    Completed 200 OK in 33ms (Views: 32.8ms | ActiveRecord: 0.0ms)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'top/main'
   resources :useres
   resources :tweets
   root 'top#main'
+
+  get 'top/main'
+  post 'top/login'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
