ad6563616ecbca326ecd6be0ab16416616f00261
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-20 11:46:58 +0000

    Completed 200 OK in 10ms (Views: 9.3ms | ActiveRecord: 0.0ms)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :users
-  resources :tweets
-  resources :top
-  resources :likes
+  #resources :tweets
+  #resources :top
+  #resources :likes
   #post 'likes', to: 'likes#create'
   #delete 'likes/:id', to: 'likes#destroy'
   
