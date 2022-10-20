bd7909213e1c4a5cadc03bb20d317614bc3ac087
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-20 10:22:22 +0000

    ActionView::Template::Error (undefined method `likes_path' for #<#<Class:0x000055d737ac44c8>:0x000055d737b202f0>):

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   post 'top/login'
   get 'top/logout'
   
-  post 'likes', to: 'likes#create'
-  delete 'likes/:id', to: 'likes#destroy'
+  #post 'likes', to: 'likes#create'
+  #delete 'likes/:id', to: 'likes#destroy'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
