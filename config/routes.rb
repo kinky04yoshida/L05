7fdf37fa7f3f56ca440f435d3d31be424eb308a6
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-20 10:11:41 +0000

    Completed 200 OK in 76ms (Views: 75.5ms | ActiveRecord: 0.0ms)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   resources :useres
   resources :tweets
-  root 'users#index'
+  root 'top#main'
 
-  #get 'top/main'
-  #post 'top/login'
-  #get 'top/logout'
+  get 'top/main'
+  post 'top/login'
+  get 'top/logout'
+  
+  post 'likes', to: 'likes#create'
+  delete 'likes/:id', to: 'likes#destroy'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
