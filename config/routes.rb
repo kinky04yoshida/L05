b91aba100ad9c0a892f10d1f0bc04bbf36745a7a
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-20 11:49:28 +0000

    Completed 200 OK in 61ms (Views: 60.6ms | ActiveRecord: 0.0ms)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'top#main'
   resources :users
   resources :tweets
+  get '/top/logout'
   #resources :top
   #resources :likes
   #post 'likes', to: 'likes#create'
