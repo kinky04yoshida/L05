7d67a3bd0d2a08ddbf16e459a479a668b0657c19
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-20 11:03:20 +0000

    NoMethodError (undefined method `id' for nil:NilClass):

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,7 +8,7 @@ class TweetsController < ApplicationController
   end
   
   def create
-    user = User.find_by(uid: @login_user)
+    user = User.find_by(uid: session[:uid])
     tweet = Tweet.new(message: params[:tweet][:message], tdate: Time.current, user_id: user.id)
     tweet.save
     redirect_to '/'
