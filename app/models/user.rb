a5fa9e29c0803d77cbfdac650d205b61b3a1a94e
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-22 21:36:30 +0000

    Completed 200 OK in 64ms (Views: 63.7ms | ActiveRecord: 0.0ms)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-    validates :uid, presence: true, uniqueness: true
-    validates :pass, presence: true
     has_many :likes
     has_many :like_tweets, through: :likes, source: :tweet
 end
