55d32226fc86a9206777f5d4dd98dbbae78ead5f
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-22 21:27:16 +0000

    ArgumentError ('/users' is not a supported controller name. This can lead to potential routing problems. See http://guides.rubyonrails.org/routing.html#specifying-a-controller-to-use):

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get '/top/logout'
   get '/top/main'
   get '/users', to: 'users#index'
-  get '/users/new', to: new_users_path
-  post 'users#new', to: 'users#create'
+  get '/users/new', to: '/users#new'
+  post '/users/new', to: 'users#create'
   get '/users/list'
   #resources :top
   resources :likes
