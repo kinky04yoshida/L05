a0b6b64d6547366234637358a948eb6109e645bb
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-22 21:35:45 +0000

    Completed 200 OK in 254ms (Views: 245.1ms | ActiveRecord: 0.0ms)

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,4 @@
 class Tweet < ApplicationRecord
-    validates :message, presence: true
-    validates :user_id, presence: true
     has_many :likes
     has_many :like_users, through: :like, source: :user
 end
