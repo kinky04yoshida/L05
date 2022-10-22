321a03f60f6ed5b9c3d1ba91b261ce828de34d78
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-22 21:10:50 +0000

    validation追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,5 @@
 class Tweet < ApplicationRecord
+    validates :message, presence: true
     has_many :likes
     has_many :like_users, through: :like, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    validates :uid, presence: true, uniqueness: true
+    validates :pass, presence: true
     has_many :likes
     has_many :like_tweets, through: :likes, source: :tweet
 end
