32bad42a4e1619d2f7731b3c655a94a3ea8257ee
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-22 23:29:38 +0000

    Completed 200 OK in 228ms (Views: 207.8ms | ActiveRecord: 3.0ms)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,9 @@
 class LikesController < ApplicationController
     def create
         tweet = Tweet.find(params[:tweet_id])
-        user = User.find_by(uid: session[:login_uid])
-        user.like_tweets << tweet
+        unless tweet.liked?(current_user)
+            tweet.like(current_user)
+        end
         redirect_to root_path
     end
 
