b2d867f5935ea4998c9ea6d6e383d70067c447c4
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-22 21:18:50 +0000

    NameError (undefined local variable or method `users_new_path' for #<ActionDispatch::Routing::Mapper:0x00007fa978c1d800>):

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/top/logout'
   get '/top/main'
   get '/users', to: 'users#index'
-  get '/users/new', to: 'users_new_path'
+  get '/users/new', to: users_new_path
   post 'users#new', to: 'users#create'
   get '/users/list'
   #resources :top
