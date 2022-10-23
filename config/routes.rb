ad83e780b929e2da31d047be923445d331def332
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-23 15:16:04 +0000

    1

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,6 @@ Rails.application.routes.draw do
   get '/users/list'
   #resources :top
   resources :likes
-  #post 'likes', to: 'likes#create'
-  #delete 'likes/:id', to: 'likes#destroy'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
