964cc2c6beb89b7fcdb7da098b3b51cb5815818e
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-23 00:57:50 +0000

    NoMethodError (undefined method `liked?' for #<Tweet:0x00007f1410c64898>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,15 @@ class UsersController < ApplicationController
     end
   end
 
+    def like(user)
+        likes.create(user_id: user.id)
+    end
+
+    def unlike(user)
+        likes.find_by(user_id: user.id).destroy
+    end
+
+    def liked?(user)
+        like_users.include?(user)
+    end
 end
