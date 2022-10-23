748806153dcf354c13f15e284eeb39ed80bd7266
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-23 00:25:31 +0000

    ActiveRecord::HasManyThroughAssociationNotFoundError (Could not find the association :like in model Tweet):

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ApplicationRecord
-    has_many :like_users, through: :like, source: :user
     has_many :likes
+    has_many :like_users, through: :like, source: :user
 
     def like(user)
         likes.create(user_id: user.id)
